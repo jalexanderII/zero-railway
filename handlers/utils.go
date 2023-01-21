064406a593c8aa8a4c6864482a9c2cf0e3997428
jalexanderII <a.joeljr@hotmail.com>
2023-01-21 16:34:28 -0500

    add user db to handler

--- handlers/utils.go
+++ handlers/utils.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jalexanderII/zero-railway/database"
@@ -18,19 +19,25 @@ type CreateResDTO struct {
 }
 
 type Handler struct {
-	Db *mongo.Collection
-	L  *logrus.Logger
-	C  context.Context
+	Db     *mongo.Collection
+	UserDb *mongo.Collection
+	L      *logrus.Logger
+	C      context.Context
 }
 
 func NewHandler(collectionName string, l *logrus.Logger) *Handler {
-	return &Handler{Db: database.GetCollection(collectionName), L: l, C: context.Background()}
+	return &Handler{
+		Db:     database.GetCollection(collectionName),
+		UserDb: database.GetCollection(os.Getenv("USER_COLLECTION")),
+		L:      l,
+		C:      context.Background(),
+	}
 }
 
 func (h *Handler) GetUserByEmail(email string) (*models.User, error) {
 	var user models.User
 	filter := bson.M{"email": email}
-	err := h.Db.FindOne(h.C, filter).Decode(&user)
+	err := h.UserDb.FindOne(h.C, filter).Decode(&user)
 	if err != nil {
 		return nil, err
 	}
